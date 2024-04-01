614d4876d7aece5fb30991fa7f0dfae3c5b21376
vph <vph.persson@gmail.com>
2024-04-02 00:30:27 +0200

    Add `Set`.

--- utils_go/iter.go
+++ utils_go/iter.go
@@ -7,3 +7,15 @@ func Map[T any, U any](s []T, f func(T) U) []U {
 	}
 	return r
 }
+
+func Set[T comparable](input []T) []T {
+	uniqueMap := make(map[T]bool)
+	var result []T
+	for _, str := range input {
+		if _, ok := uniqueMap[str]; !ok {
+			uniqueMap[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+}
