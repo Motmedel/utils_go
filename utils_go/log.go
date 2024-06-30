16e513d00402d3070c6fbcd2b007cd149bb3e0ee
vph <vph.persson@gmail.com>
2024-06-30 12:04:01 +0200

    Add error methods.

--- utils_go/log.go
+++ utils_go/log.go
@@ -3,7 +3,40 @@ package utils_go
 import (
 	"io"
 	"log/slog"
+	"reflect"
 )
+
+func AttrsFromMap(m map[string]any) []any {
+	var attrs []any
+
+	for key, value := range m {
+		switch typedValue := value.(type) {
+		case string:
+			attrs = append(attrs, slog.String(key, typedValue))
+		case int:
+			attrs = append(attrs, slog.Int(key, typedValue))
+		case float64:
+			attrs = append(attrs, slog.Float64(key, typedValue))
+		case bool:
+			attrs = append(attrs, slog.Bool(key, typedValue))
+		case map[string]any:
+			attrs = append(attrs, slog.Group(key, AttrsFromMap(typedValue)...))
+		default:
+			attrs = append(attrs, slog.Any(key, value))
+		}
+	}
+
+	return attrs
+}
+
+func MakeErrorGroup(err error) *slog.Attr {
+	group := slog.Group(
+		"error",
+		slog.String("message", err.Error()),
+		slog.String("type", reflect.TypeOf(err).String()),
+	)
+	return &group
+}
 
 func GCPReplaceAttr(groups []string, attr slog.Attr) slog.Attr {
 	if len(groups) > 0 {
