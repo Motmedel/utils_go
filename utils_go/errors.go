fc71ed8889019e686eb455936e173b20dd6ba71d
vph <vph.persson@gmail.com>
2024-07-24 13:59:10 +0200

    Correctly implement `InputError`.

--- utils_go/errors.go
+++ utils_go/errors.go
@@ -1,24 +1,21 @@
 package utils_go
 
-import "encoding/json"
-
 type InputErrorI interface {
 	Error() string
 	GetInput() any
 }
 
 type InputError struct {
-	error
+	Err   error
 	Input any
+}
+
+func (inputError *InputError) Error() string {
+	return inputError.Err.Error()
 }
 
 func (inputError *InputError) GetInput() any {
 	return inputError.Input
-}
-
-type JsonSyntaxError struct {
-	*json.SyntaxError
-	*InputError
 }
 
 type CauseErrorI interface {
