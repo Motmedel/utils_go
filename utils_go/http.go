6c256c4b0c48e19f7e26df66c07f83b2c9a89982
vph <vph.persson@gmail.com>
2024-08-04 15:02:45 +0200

    Improve HTTP parsing.

--- utils_go/http.go
+++ utils_go/http.go
@@ -7,24 +7,32 @@ import (
 )
 
 func ParseHttpRequestData(requestBytes []byte) (*http.Request, error) {
+	if len(requestBytes) == 0 {
+		return nil, nil
+	}
+
 	request, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(requestBytes)))
 	if err != nil {
-		// TODO: This could be an InputError? I could make some aggregated struct in `utils_go` + stack
-		return nil, &CauseError{
+		return nil, &InputError{
 			Message: "An error occurred when reading and parsing data as an HTTP request.",
 			Cause:   err,
+			Input:   requestBytes,
 		}
 	}
 	return request, nil
 }
 
 func ParseHttpResponseData(responseBytes []byte) (*http.Response, error) {
+	if len(responseBytes) == 0 {
+		return nil, nil
+	}
+
 	response, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(responseBytes)), nil)
 	if err != nil {
-		// TODO: This could be an InputError? I could make some aggregated struct in `utils_go` + stack
-		return nil, &CauseError{
+		return nil, &InputError{
 			Message: "An error occurred when reading and parsing data as an HTTP response.",
 			Cause:   err,
+			Input:   responseBytes,
 		}
 	}
 	return response, nil
