8f20d924bfc5b67bb9d1ae7a3f0d7eb3993d2f11
vph <vph.persson@gmail.com>
2024-09-26 20:23:10 +0200

    Add jsonify.

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -2,8 +2,12 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/Motmedel/utils_go/pkg/errors"
 	"io"
+	"reflect"
+	"slices"
+	"strings"
 )
 
 func DecodeJson[T any](reader io.Reader) (T, error) {
@@ -25,17 +29,67 @@ func DecodeJson[T any](reader io.Reader) (T, error) {
 	return obj, err
 }
 
-func JsonObjectToMap[T any, S comparable](value T) (map[S]any, error) {
-	var valueBytes []byte
-	var err error
+func Jsonify(input any) any {
+	inputValue := reflect.ValueOf(input)
 
-	valueBytes, err = json.Marshal(value)
-	if err != nil {
-		return nil, err
+	switch inputValue.Kind() {
+	case reflect.Ptr:
+		return Jsonify(inputValue.Elem().Interface())
+	case reflect.Struct:
+		structMap := make(map[string]any)
+		inputValueType := inputValue.Type()
+		for i := 0; i < inputValue.NumField(); i++ {
+			field := inputValueType.Field(i)
+			if field.PkgPath != "" && !field.Anonymous {
+				continue
+			}
+
+			tag := field.Tag.Get("json")
+			if tag == "-" {
+				continue
+			}
+
+			value := inputValue.Field(i)
+
+			tagSplit := strings.Split(tag, ",")
+			hasOmitEmpty := slices.Contains(tagSplit, "omitempty")
+
+			if value.IsZero() && hasOmitEmpty {
+				continue
+			}
+
+			if field.Anonymous {
+				if embeddedValueMap, ok := Jsonify(value.Interface()).(map[string]any); ok {
+					for embeddedValueMapKey, embeddedValueMapValue := range embeddedValueMap {
+						structMap[embeddedValueMapKey] = embeddedValueMapValue
+					}
+				} else {
+					continue
+				}
+			} else {
+				key := field.Name
+				if nameIndex := slices.IndexFunc(tagSplit, func(s string) bool { return s != "omitempty" }); nameIndex >= 0 {
+					key = tagSplit[nameIndex]
+				}
+
+				structMap[key] = Jsonify(value.Interface())
+			}
+		}
+		return structMap
+	case reflect.Slice, reflect.Array:
+		elements := make([]any, inputValue.Len())
+		for i := 0; i < inputValue.Len(); i++ {
+			elements[i] = Jsonify(inputValue.Index(i).Interface())
+		}
+		return elements
+	case reflect.Map:
+		mapKeys := inputValue.MapKeys()
+		m := make(map[string]any)
+		for _, mapKey := range mapKeys {
+			m[fmt.Sprintf("%v", mapKey)] = Jsonify(inputValue.MapIndex(mapKey).Interface())
+		}
+		return m
+	default:
+		return inputValue.Interface()
 	}
-
-	var valueMap map[S]any
-	err = json.Unmarshal(valueBytes, &valueMap)
-
-	return valueMap, err
 }
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -37,24 +37,9 @@ func CtxWithLogger(ctx context.Context, logger *slog.Logger) context.Context {
 
 func AttrsFromMap(m map[string]any) []any {
 	var attrs []any
-
 	for key, value := range m {
-		switch typedValue := value.(type) {
-		case string:
-			attrs = append(attrs, slog.String(key, typedValue))
-		case int:
-			attrs = append(attrs, slog.Int(key, typedValue))
-		case float64:
-			attrs = append(attrs, slog.Float64(key, typedValue))
-		case bool:
-			attrs = append(attrs, slog.Bool(key, typedValue))
-		case map[string]any:
-			attrs = append(attrs, slog.Group(key, AttrsFromMap(typedValue)...))
-		default:
-			attrs = append(attrs, slog.Any(key, value))
-		}
+		attrs = append(attrs, slog.Any(key, value))
 	}
-
 	return attrs
 }
 
