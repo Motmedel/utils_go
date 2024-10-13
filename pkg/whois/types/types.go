fddfb56e5206b12ae7498cd81df3d0f4ddc1e9e2
vph <vph.persson@gmail.com>
2024-10-13 14:25:26 +0200

    Improve whois context.

--- pkg/whois/types/types.go
+++ pkg/whois/types/types.go
@@ -1,11 +1,13 @@
 package types
 
 type WhoisContext struct {
-	ServerAddress string
-	ServerPort    int
-	ClientAddress string
-	ClientPort    int
-	Transport     string
-	RequestData   []byte
-	ResponseData  []byte
+	ServerAddress   string
+	ServerIpAddress string
+	ServerPort      int
+	ClientAddress   string
+	ClientIpAddress string
+	ClientPort      int
+	Transport       string
+	RequestData     []byte
+	ResponseData    []byte
 }
