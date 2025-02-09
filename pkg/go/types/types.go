cd50a6c6ea03a8a2eb1e3907fd5939a21591e69a
vph <vph.persson@gmail.com>
2025-02-09 17:55:42 +0100

    Add BuildEvent

--- pkg/go/types/types.go
+++ pkg/go/types/types.go
@@ -1,10 +1,17 @@
 package types
 
+type BuildEvent struct {
+	ImportPath string `json:"ImportPath,omitempty"`
+	Action     string `json:"Action,omitempty"`
+	Output     string `json:"Output,omitempty"`
+}
+
 type TestEvent struct {
-	Time    string  `json:"Time,omitempty"`
-	Action  string  `json:"Action,omitempty"`
-	Package string  `json:"Package,omitempty"`
-	Test    string  `json:"Test,omitempty"`
-	Elapsed float64 `json:"Elapsed,omitempty"`
-	Output  string  `json:"Output,omitempty"`
+	Time        string  `json:"Time,omitempty"`
+	Action      string  `json:"Action,omitempty"`
+	Package     string  `json:"Package,omitempty"`
+	Test        string  `json:"Test,omitempty"`
+	Elapsed     float64 `json:"Elapsed,omitempty"`
+	Output      string  `json:"Output,omitempty"`
+	FailedBuild string  `json:"FailedBuild,omitempty"`
 }
