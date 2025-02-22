169f7221e764f2bf8a9fe5780823fd806e5ffb7f
vph <vph.persson@gmail.com>
2025-02-22 18:04:07 +0100

    Add request id context key.

--- pkg/http/context/context.go
+++ pkg/http/context/context.go
@@ -1,5 +1,9 @@
 package context
 
+type requestIdContextType struct{}
+
+var RequestIdContextKey = &requestIdContextType{}
+
 type httpContextContextType struct{}
 
 var HttpContextContextKey httpContextContextType
