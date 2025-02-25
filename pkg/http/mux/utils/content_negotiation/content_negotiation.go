353af55c88081a57e1ba6c06eabbdd58bfbfe0f6
vph <vph.persson@gmail.com>
2025-02-25 15:52:00 +0100

    Fix dependencies

---
+++ pkg/http/mux/utils/content_negotiation/content_negotiation.go
@@ -0,0 +1,38 @@
+package content_negotiation
+
+import (
+	"fmt"
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
+	motmedelHttpHeadersParsingAccept "github.com/Motmedel/utils_go/pkg/http/parsing/headers/accept"
+	motmedelHttpHeadersParsingAcceptEncoding "github.com/Motmedel/utils_go/pkg/http/parsing/headers/accept_encoding"
+	motmedelHttpTypes "github.com/Motmedel/utils_go/pkg/http/types"
+	"net/http"
+)
+
+func GetContentNegotiation(requestHeader http.Header, strict bool) (*motmedelHttpTypes.ContentNegotiation, error) {
+	if len(requestHeader) == 0 {
+		return nil, nil
+	}
+
+	var contentNegotiation motmedelHttpTypes.ContentNegotiation
+
+	if acceptValue := requestHeader.Get("Accept"); acceptValue != "" {
+		acceptData := []byte(acceptValue)
+		accept, err := motmedelHttpHeadersParsingAccept.ParseAccept(acceptData)
+		if err != nil && strict {
+			return nil, motmedelErrors.MakeError(fmt.Errorf("parse accept: %w", err), acceptData)
+		}
+		contentNegotiation.Accept = accept
+	}
+
+	if acceptEncodingValue := requestHeader.Get("Accept-Encoding"); acceptEncodingValue != "" {
+		acceptEncodingData := []byte(acceptEncodingValue)
+		acceptEncoding, err := motmedelHttpHeadersParsingAcceptEncoding.ParseAcceptEncoding(acceptEncodingData)
+		if err != nil && strict {
+			return nil, motmedelErrors.MakeError(fmt.Errorf("parse accept encoding: %w", err), acceptEncodingData)
+		}
+		contentNegotiation.AcceptEncoding = acceptEncoding
+	}
+
+	return &contentNegotiation, nil
+}
--- pkg/http/utils/utils.go
+++ pkg/http/utils/utils.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	motmedelHttpErrors "github.com/Motmedel/utils_go/pkg/http/errors"
-	motmedelHttpHeadersParsingAccept "github.com/Motmedel/utils_go/pkg/http/parsing/headers/accept"
-	motmedelHttpHeadersParsingAcceptEncoding "github.com/Motmedel/utils_go/pkg/http/parsing/headers/accept_encoding"
 	motmedelHttpTypes "github.com/Motmedel/utils_go/pkg/http/types"
 	"io"
 	"net/http"
@@ -300,34 +298,6 @@ func SendJsonRequestResponse[T any, U any](
 	}
 
 	return responseValue, httpContext, nil
-}
-
-func GetContentNegotiation(requestHeader http.Header, strict bool) (*motmedelHttpTypes.ContentNegotiation, error) {
-	if len(requestHeader) == 0 {
-		return nil, nil
-	}
-
-	var contentNegotiation motmedelHttpTypes.ContentNegotiation
-
-	if acceptValue := requestHeader.Get("Accept"); acceptValue != "" {
-		acceptData := []byte(acceptValue)
-		accept, err := motmedelHttpHeadersParsingAccept.ParseAccept(acceptData)
-		if err != nil && strict {
-			return nil, motmedelErrors.MakeError(fmt.Errorf("parse accept: %w", err), acceptData)
-		}
-		contentNegotiation.Accept = accept
-	}
-
-	if acceptEncodingValue := requestHeader.Get("Accept-Encoding"); acceptEncodingValue != "" {
-		acceptEncodingData := []byte(acceptEncodingValue)
-		acceptEncoding, err := motmedelHttpHeadersParsingAcceptEncoding.ParseAcceptEncoding(acceptEncodingData)
-		if err != nil && strict {
-			return nil, motmedelErrors.MakeError(fmt.Errorf("parse accept encoding: %w", err), acceptEncodingData)
-		}
-		contentNegotiation.AcceptEncoding = acceptEncoding
-	}
-
-	return &contentNegotiation, nil
 }
 
 func GetMatchingContentEncoding(
