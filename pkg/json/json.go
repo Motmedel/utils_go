92449fea161c641dd6336936ad7fde3d6853ae9b
vph <vph.persson@gmail.com>
2025-03-16 23:52:24 +0100

    Add json conversion

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -3,7 +3,7 @@ package json
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/Motmedel/utils_go/pkg/errors"
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	"io"
 	"reflect"
 	"slices"
@@ -15,18 +15,42 @@ func DecodeJson[T any](reader io.Reader) (T, error) {
 
 	data, err := io.ReadAll(reader)
 	if err != nil {
-		return obj, &errors.Error{Message: "An error occurred when reading the data.", Cause: err}
+		return obj, motmedelErrors.New(fmt.Errorf("io read all: %w", err))
 	}
 
 	if err := json.Unmarshal(data, &obj); err != nil {
-		return obj, &errors.Error{
-			Message: "An error occurred when unmarshalling the data.",
-			Cause:   err,
-			Input:   data,
-		}
+		return obj, motmedelErrors.New(fmt.Errorf("json unmarshal: %w", err), data)
 	}
 
 	return obj, err
+}
+
+func ObjectToMap(object any) (map[string]any, error) {
+	data, err := json.Marshal(object)
+	if err != nil {
+		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("json marshal: %w", err), object)
+	}
+
+	var objectMap map[string]any
+	if err = json.Unmarshal(data, &objectMap); err != nil {
+		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("json unmarshal: %w", err), data)
+	}
+
+	return objectMap, nil
+}
+
+func ObjectToBytes(object any) ([]byte, error) {
+	objectMap, err := ObjectToMap(object)
+	if err != nil {
+		return nil, motmedelErrors.New(fmt.Errorf("object to map: %w", err), object)
+	}
+
+	data, err := json.Marshal(objectMap)
+	if err != nil {
+		return nil, motmedelErrors.New(fmt.Errorf("json marshal: %w", err), objectMap)
+	}
+
+	return data, nil
 }
 
 func Jsonify(input any) any {
