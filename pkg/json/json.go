3714b10ec56ba24ce597eecaf6283bcbe3580585
vph <vph.persson@gmail.com>
2025-03-16 23:53:40 +0100

    Remove jsonify

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	"io"
-	"reflect"
-	"slices"
-	"strings"
 )
 
 func DecodeJson[T any](reader io.Reader) (T, error) {
@@ -51,80 +48,4 @@ func ObjectToBytes(object any) ([]byte, error) {
 	}
 
 	return data, nil
-}
-
-func Jsonify(input any) any {
-	inputValue := reflect.ValueOf(input)
-
-	switch inputValue.Kind() {
-	case reflect.Ptr:
-		if inputValue.IsNil() {
-			return nil
-		}
-
-		elem := inputValue.Elem()
-		if elem.Kind() == reflect.Invalid {
-			return nil
-		}
-		return Jsonify(elem.Interface())
-	case reflect.Struct:
-		structMap := make(map[string]any)
-		inputValueType := inputValue.Type()
-		for i := 0; i < inputValue.NumField(); i++ {
-			field := inputValueType.Field(i)
-			if field.PkgPath != "" && !field.Anonymous {
-				continue
-			}
-
-			tag := field.Tag.Get("json")
-			if tag == "-" {
-				continue
-			}
-
-			value := inputValue.Field(i)
-
-			tagSplit := strings.Split(tag, ",")
-			hasOmitEmpty := slices.Contains(tagSplit, "omitempty")
-
-			if value.IsZero() && hasOmitEmpty {
-				continue
-			}
-
-			if field.Anonymous {
-				if embeddedValueMap, ok := Jsonify(value.Interface()).(map[string]any); ok {
-					for embeddedValueMapKey, embeddedValueMapValue := range embeddedValueMap {
-						structMap[embeddedValueMapKey] = embeddedValueMapValue
-					}
-				} else {
-					continue
-				}
-			} else {
-				key := field.Name
-				if tagName := tagSplit[0]; tagName != "" {
-					key = tagName
-				}
-
-				structMap[key] = Jsonify(value.Interface())
-			}
-		}
-		return structMap
-	case reflect.Slice, reflect.Array:
-		elements := make([]any, inputValue.Len())
-		for i := 0; i < inputValue.Len(); i++ {
-			elements[i] = Jsonify(inputValue.Index(i).Interface())
-		}
-		return elements
-	case reflect.Map:
-		mapKeys := inputValue.MapKeys()
-		m := make(map[string]any)
-		for _, mapKey := range mapKeys {
-			m[fmt.Sprintf("%v", mapKey)] = Jsonify(inputValue.MapIndex(mapKey).Interface())
-		}
-		return m
-	default:
-		if inputValue.Kind() == reflect.Invalid {
-			return nil
-		}
-		return inputValue.Interface()
-	}
 }
