dccd508417e82c1d2264833fcbce83b4419bc799
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-06-17 01:10:15 +0200

    Add err

--- pkg/net/errors/errors.go
+++ pkg/net/errors/errors.go
@@ -8,4 +8,5 @@ var (
 	ErrNotOnSubnetBoundaries = errors.New("the start and end IP addresses are not on the exact subnet boundaries")
 	ErrStartAfterEnd         = errors.New("the start IP address does not come before the end IP address")
 	ErrUnexpectedIpVersion   = errors.New("unexpected IP version")
+	ErrNilConn               = errors.New("nil conn")
 )
