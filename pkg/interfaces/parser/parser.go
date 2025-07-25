8db4d1293ff62c5f194c28ce6b2df6901093bad4
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-07-25 12:25:45 +0200

    Add parser interface

---
+++ pkg/interfaces/parser/parser.go
@@ -0,0 +1,11 @@
+package parser
+
+type Parser[T any, U any] interface {
+	Parse(U) (T, error)
+}
+
+type ParserFunction[T any, U any] func(U) (T, error)
+
+func (pf ParserFunction[T, U]) Parse(input U) (T, error) {
+	return pf(input)
+}
