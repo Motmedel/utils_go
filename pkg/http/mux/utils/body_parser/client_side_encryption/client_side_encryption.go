d767fc47a5f0fa2a86bcee0159974cfd5de1a2cd
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-10-21 00:52:42 +0200

    Add header parser

--- pkg/http/mux/utils/body_parser/client_side_encryption/client_side_encryption.go
+++ pkg/http/mux/utils/body_parser/client_side_encryption/client_side_encryption.go
@@ -5,20 +5,110 @@ import (
 	"net/http"
 
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
+	motmedelHttpErrors "github.com/Motmedel/utils_go/pkg/http/errors"
 	muxErrors "github.com/Motmedel/utils_go/pkg/http/mux/errors"
 	"github.com/Motmedel/utils_go/pkg/http/mux/interfaces/body_processor"
 	"github.com/Motmedel/utils_go/pkg/http/mux/types/response_error"
+	"github.com/Motmedel/utils_go/pkg/http/problem_detail"
 	"github.com/Motmedel/utils_go/pkg/utils"
 	"github.com/go-jose/go-jose/v4"
 )
 
-type CseBodyParser struct {
+type HeaderRequestParser struct {
+	Header            string
+	KeyAlgorithm      jose.KeyAlgorithm
+	ContentEncryption jose.ContentEncryption
+}
+
+func (parser *HeaderRequestParser) Parse(request *http.Request) (jose.Encrypter, *response_error.ResponseError) {
+	if request == nil {
+		return nil, &response_error.ResponseError{
+			ServerError: motmedelErrors.NewWithTrace(motmedelHttpErrors.ErrNilHttpRequest),
+		}
+	}
+
+	requestHeader := request.Header
+	if requestHeader == nil {
+		return nil, &response_error.ResponseError{
+			ServerError: motmedelErrors.NewWithTrace(motmedelHttpErrors.ErrNilHttpRequestHeader),
+		}
+	}
+
+	_, ok := requestHeader[parser.Header]
+	if !ok {
+		return nil, &response_error.ResponseError{
+			ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+				fmt.Sprintf("Missing %q header.", parser.Header),
+				nil,
+			),
+		}
+	}
+	clientJwkRaw := requestHeader.Get(parser.Header)
+	if clientJwkRaw == "" {
+		return nil, &response_error.ResponseError{
+			ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+				fmt.Sprintf("Empty %q header.", parser.Header),
+				nil,
+			),
+		}
+	}
+
+	var clientJwk jose.JSONWebKey
+	if err := clientJwk.UnmarshalJSON([]byte(clientJwkRaw)); err != nil {
+		return nil, &response_error.ResponseError{
+			ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+				fmt.Sprintf("Invalid %q header.", parser.Header),
+				nil,
+			),
+			ClientError: motmedelErrors.NewWithTrace(
+				fmt.Errorf("json web key unmarshal json: %w", err),
+				clientJwkRaw,
+			),
+		}
+	}
+	clientJwkKey := clientJwk.Key
+	if clientJwkKey == nil {
+		return nil, &response_error.ResponseError{
+			ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+				"Missing client public JWK key.",
+				nil,
+			),
+		}
+	}
+
+	clientJwkKeyId := clientJwk.KeyID
+	responseEncrypter, err := jose.NewEncrypter(
+		parser.ContentEncryption,
+		jose.Recipient{
+			Algorithm: parser.KeyAlgorithm,
+			Key:       clientJwkKey,
+			KeyID:     clientJwkKeyId,
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, &response_error.ResponseError{
+			ProblemDetail: problem_detail.MakeBadRequestProblemDetail(
+				"Malformed client public JWK key.",
+				nil,
+			),
+			ClientError: motmedelErrors.NewWithTrace(
+				fmt.Errorf("jose new encrypter: %w", err),
+				clientJwkKey, clientJwkKeyId,
+			),
+		}
+	}
+
+	return responseEncrypter, nil
+}
+
+type BodyParser struct {
 	PrivateKey        any
 	KeyAlgorithm      jose.KeyAlgorithm
 	ContentEncryption jose.ContentEncryption
 }
 
-func (bodyParser *CseBodyParser) Parse(_ *http.Request, body []byte) ([]byte, *response_error.ResponseError) {
+func (bodyParser *BodyParser) Parse(_ *http.Request, body []byte) ([]byte, *response_error.ResponseError) {
 	jwe, err := jose.ParseEncrypted(
 		string(body),
 		[]jose.KeyAlgorithm{bodyParser.KeyAlgorithm},
@@ -43,15 +133,15 @@ func (bodyParser *CseBodyParser) Parse(_ *http.Request, body []byte) ([]byte, *r
 	return plaintext, nil
 }
 
-type CseBodyParserWithProcessor[T any] struct {
-	CseBodyParser
+type BodyParserWithProcessor[T any] struct {
+	BodyParser
 	Processor body_processor.BodyProcessor[T, []byte]
 }
 
-func (bodyParser *CseBodyParserWithProcessor[T]) Parse(request *http.Request, body []byte) (T, *response_error.ResponseError) {
+func (bodyParser *BodyParserWithProcessor[T]) Parse(request *http.Request, body []byte) (T, *response_error.ResponseError) {
 	var zero T
 
-	parser := bodyParser.CseBodyParser
+	parser := bodyParser.BodyParser
 	if utils.IsNil(parser) {
 		return zero, &response_error.ResponseError{ServerError: muxErrors.ErrNilBodyParser}
 	}
--- pkg/http/mux/utils/body_parser/client_side_encryption/go.mod
+++ pkg/http/mux/utils/body_parser/client_side_encryption/go.mod
@@ -5,10 +5,8 @@ go 1.25
 require (
 	github.com/Motmedel/utils_go v0.0.277
 	github.com/Motmedel/utils_go/pkg/http/mux v0.0.107
+	github.com/Motmedel/utils_go/pkg/http/problem_detail v1.0.5
 	github.com/go-jose/go-jose/v4 v4.1.3
 )
 
-require (
-	github.com/Motmedel/utils_go/pkg/http/problem_detail v1.0.5 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-)
+require github.com/google/uuid v1.6.0 // indirect
