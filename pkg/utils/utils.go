d2cd2e9c93cc52c8ec7350d2f5bcd30931033bc2
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-11-28 12:14:06 +0100

    Add convert slice

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,8 +2,9 @@ package utils
 
 import (
 	"fmt"
-	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	"reflect"
+
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 )
 
 func Convert[T any](value any) (T, error) {
@@ -16,6 +17,34 @@ func Convert[T any](value any) (T, error) {
 	}
 
 	return convertedValue, nil
+}
+
+func ConvertSlice[T any](value any) ([]T, error) {
+	var zero []T
+
+	if typedValue, ok := value.([]T); ok {
+		return typedValue, nil
+	}
+
+	if anySlice, ok := value.([]any); ok {
+		typedSlice := make([]T, len(anySlice))
+		for i, sliceValue := range anySlice {
+			typedSliceValue, ok := sliceValue.(T)
+			if !ok {
+				return nil, motmedelErrors.NewWithTrace(
+					fmt.Errorf("%w: %T (slice value)", motmedelErrors.ErrConversionNotOk, value),
+					value,
+				)
+			}
+			typedSlice[i] = typedSliceValue
+		}
+		return typedSlice, nil
+	}
+
+	return zero, motmedelErrors.NewWithTrace(
+		fmt.Errorf("%w: %T", motmedelErrors.ErrConversionNotOk, value),
+		value,
+	)
 }
 
 func ConvertToNonZero[T comparable](value any) (T, error) {
