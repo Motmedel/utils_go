01690f7ada3e3c20d0dd4f9fd30ecbc9280c8655
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-12-16 15:47:26 +0100

    Fix TxAuthorizer

--- pkg/database/sql/types/tx_authorizer/tx_authorizer.go
+++ pkg/database/sql/types/tx_authorizer/tx_authorizer.go
@@ -6,15 +6,15 @@ import (
 )
 
 type TxAuthorizer interface {
-	Authorized(context.Context, *sql.Tx) (bool, error)
+	Authorized(context.Context, string, *sql.Tx) (bool, error)
 }
 
-type TxAuthorizerFunction func(context.Context, *sql.Tx) (bool, error)
+type TxAuthorizerFunction func(context.Context, string, *sql.Tx) (bool, error)
 
-func (f TxAuthorizerFunction) Authorized(ctx context.Context, tx *sql.Tx) (bool, error) {
-	return f(ctx, tx)
+func (f TxAuthorizerFunction) Authorized(ctx context.Context, id string, tx *sql.Tx) (bool, error) {
+	return f(ctx, id, tx)
 }
 
-func New(fn func(context.Context, *sql.Tx) (bool, error)) TxAuthorizer {
+func New(fn func(context.Context, string, *sql.Tx) (bool, error)) TxAuthorizer {
 	return TxAuthorizerFunction(fn)
 }
