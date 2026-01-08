48bf165fd75ee004a0514fa51e2c4bd214199b84
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-01-08 15:47:05 +0100

    Add patch frame src

--- pkg/http/utils/content_security_policy/content_security_policy.go
+++ pkg/http/utils/content_security_policy/content_security_policy.go
@@ -4,33 +4,38 @@ import (
 	"net/url"
 	"slices"
 
-	"github.com/Motmedel/utils_go/pkg/http/types/content_security_policy"
+	csp "github.com/Motmedel/utils_go/pkg/http/types/content_security_policy"
 )
 
-func PatchCspConnectSrcWithHostSrc(contentSecurityPolicy *content_security_policy.ContentSecurityPolicy, hostUrls ...*url.URL) {
-	if contentSecurityPolicy == nil {
-		return
-	}
-
-	var hostSources []content_security_policy.SourceI
+func buildHostSources(hostUrls ...*url.URL) []csp.SourceI {
+	var hostSources []csp.SourceI
 	for _, hostUrl := range hostUrls {
 		if hostUrl == nil {
 			continue
 		}
-		if hostSource := content_security_policy.HostSourceFromUrl(hostUrl); hostSource != nil {
+		if hostSource := csp.HostSourceFromUrl(hostUrl); hostSource != nil {
 			hostSources = append(hostSources, hostSource)
 		}
 	}
+	return hostSources
+}
+
+func PatchCspConnectSrcWithHostSrc(contentSecurityPolicy *csp.ContentSecurityPolicy, hostUrls ...*url.URL) {
+	if contentSecurityPolicy == nil {
+		return
+	}
+
+	hostSources := buildHostSources(hostUrls...)
 
 	if len(hostSources) == 0 {
 		return
 	}
 
-	connectSrcDirective := &content_security_policy.ConnectSrcDirective{
-		SourceDirective: content_security_policy.SourceDirective{
+	connectSrcDirective := &csp.ConnectSrcDirective{
+		SourceDirective: csp.SourceDirective{
 			Sources: slices.Concat(
-				[]content_security_policy.SourceI{
-					&content_security_policy.KeywordSource{Keyword: "self"},
+				[]csp.SourceI{
+					&csp.KeywordSource{Keyword: "self"},
 				},
 				hostSources,
 			),
@@ -45,11 +50,48 @@ func PatchCspConnectSrcWithHostSrc(contentSecurityPolicy *content_security_polic
 
 		for _, hostSource := range hostSources {
 			if _, found := sourceMap[hostSource.String()]; !found {
-
 				existingConnectSrcDirective.Sources = append(existingConnectSrcDirective.Sources, hostSource)
 			}
 		}
 	} else {
 		contentSecurityPolicy.Directives = append(contentSecurityPolicy.Directives, connectSrcDirective)
+	}
+}
+
+func PatchCspFrameSrcWithHostSrc(contentSecurityPolicy *csp.ContentSecurityPolicy, hostUrls ...*url.URL) {
+	if contentSecurityPolicy == nil {
+		return
+	}
+
+	hostSources := buildHostSources(hostUrls...)
+
+	if len(hostSources) == 0 {
+		return
+	}
+
+	frameSrcDirective := &csp.FrameSrcDirective{
+		SourceDirective: csp.SourceDirective{
+			Sources: slices.Concat(
+				[]csp.SourceI{
+					&csp.KeywordSource{Keyword: "self"},
+				},
+				hostSources,
+			),
+		},
+	}
+
+	if existingFrameSrcDirective := contentSecurityPolicy.GetFrameSrc(); existingFrameSrcDirective != nil {
+		sourceMap := make(map[string]struct{})
+		for _, source := range existingFrameSrcDirective.Sources {
+			sourceMap[source.String()] = struct{}{}
+		}
+
+		for _, hostSource := range hostSources {
+			if _, found := sourceMap[hostSource.String()]; !found {
+				existingFrameSrcDirective.Sources = append(existingFrameSrcDirective.Sources, hostSource)
+			}
+		}
+	} else {
+		contentSecurityPolicy.Directives = append(contentSecurityPolicy.Directives, frameSrcDirective)
 	}
 }
