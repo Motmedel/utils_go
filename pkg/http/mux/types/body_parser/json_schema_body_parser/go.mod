bbe52d54b3dd84bb83937e64d4b8bcb2f7b09211
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-01-23 17:43:54 +0100

    Fix json schema body parser

--- pkg/http/mux/types/body_parser/json_schema_body_parser/go.mod
+++ pkg/http/mux/types/body_parser/json_schema_body_parser/go.mod
@@ -4,16 +4,14 @@ go 1.25
 
 require (
 	github.com/Motmedel/utils_go v0.0.350
-	github.com/Motmedel/utils_go/pkg/http/mux v0.0.143
+	github.com/Motmedel/utils_go/pkg/http/mux v0.0.145
 	github.com/Motmedel/utils_go/pkg/http/problem_detail v1.0.5
-	github.com/Motmedel/utils_go/pkg/json/schema v0.0.6
-	github.com/altshiftab/jsonschema v0.0.8
+	github.com/altshiftab/jsonschema v0.0.10
 )
 
 require (
-	github.com/Motmedel/utils_go/pkg/net/domain_breakdown v0.0.5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/vphpersson/type_generation v0.0.18 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
+	github.com/vphpersson/type_generation v0.0.19 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 )
--- pkg/http/mux/types/body_parser/json_schema_body_parser/json_schema_body_parser.go
+++ pkg/http/mux/types/body_parser/json_schema_body_parser/json_schema_body_parser.go
@@ -8,38 +8,43 @@ import (
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
 	muxErrors "github.com/Motmedel/utils_go/pkg/http/mux/errors"
 	"github.com/Motmedel/utils_go/pkg/http/mux/types/body_parser"
+	"github.com/Motmedel/utils_go/pkg/http/mux/types/body_parser/json_body_parser"
 	"github.com/Motmedel/utils_go/pkg/http/mux/types/response_error"
-	muxUtils "github.com/Motmedel/utils_go/pkg/http/mux/utils"
-	muxUtilsJson "github.com/Motmedel/utils_go/pkg/http/mux/utils/json"
 	"github.com/Motmedel/utils_go/pkg/http/problem_detail"
-	motmedelJsonSchema "github.com/Motmedel/utils_go/pkg/json/schema"
 	"github.com/Motmedel/utils_go/pkg/utils"
 	jsonschemaErrors "github.com/altshiftab/jsonschema/pkg/errors"
 	"github.com/altshiftab/jsonschema/pkg/jsonschema"
 )
 
+var jsonMapBodyParser = json_body_parser.New[map[string]any]()
+
 type Parser[T any] struct {
-	body_parser.BodyParser[T]
-	Schema *jsonschema.Schema
+	schema     *jsonschema.Schema
+	bodyParser body_parser.BodyParser[T]
 }
 
 func (p *Parser[T]) Parse(request *http.Request, body []byte) (T, *response_error.ResponseError) {
 	var zero T
 
-	schema := p.Schema
+	schema := p.schema
 	if schema == nil {
 		return zero, &response_error.ResponseError{ServerError: motmedelErrors.NewWithTrace(jsonschemaErrors.ErrNilSchema)}
 	}
 
-	dataMap, responseError := muxUtilsJson.ParseJsonBody[map[string]any](body)
+	bodyParser := p.bodyParser
+	if utils.IsNil(bodyParser) {
+		return zero, &response_error.ResponseError{ServerError: motmedelErrors.NewWithTrace(muxErrors.ErrNilBodyParser)}
+	}
+
+	dataMap, responseError := jsonMapBodyParser.Parse(request, body)
 	if responseError != nil {
 		return zero, responseError
 	}
 
-	if err := motmedelJsonSchema.Validate(dataMap, schema); err != nil {
+	if err := schema.Validate(dataMap); err != nil {
 		wrappedErr := motmedelErrors.New(fmt.Errorf("validate (input): %w", err), dataMap, schema)
 
-		var validateError *motmedelJsonSchema.ValidateError
+		var validateError *jsonschemaErrors.ValidateError
 		if errors.As(err, &validateError) {
 			return zero, &response_error.ResponseError{
 				// TODO: The error messages could be made nicer.
@@ -55,13 +60,8 @@ func (p *Parser[T]) Parse(request *http.Request, body []byte) (T, *response_erro
 		return zero, &response_error.ResponseError{ServerError: wrappedErr}
 	}
 
-	parser := p.BodyParser
-	if utils.IsNil(parser) {
-		return zero, &response_error.ResponseError{ServerError: motmedelErrors.NewWithTrace(muxErrors.ErrNilBodyParser)}
-	}
-
 	var result T
-	result, responseError = parser.Parse(request, body)
+	result, responseError = bodyParser.Parse(request, body)
 	if responseError != nil {
 		return zero, responseError
 	}
@@ -69,15 +69,19 @@ func (p *Parser[T]) Parse(request *http.Request, body []byte) (T, *response_erro
 	return result, nil
 }
 
-func NewWithSchema[T any](schema *jsonschema.Schema) *Parser[T] {
-	return &Parser[T]{BodyParser: muxUtils.MakeJsonBodyParser[T](), Schema: schema}
+func NewWithSchema[T any](schema *jsonschema.Schema) (*Parser[T], error) {
+	if schema == nil {
+		return nil, motmedelErrors.NewWithTrace(jsonschemaErrors.ErrNilSchema)
+	}
+
+	return &Parser[T]{schema: schema, bodyParser: json_body_parser.New[T]()}, nil
 }
 
 func New[T any]() (*Parser[T], error) {
-	schema, err := jsonschema.FromType[T]()
+	schema, err := jsonschema.NewFromType[T]()
 	if err != nil {
 		return nil, fmt.Errorf("schema new: %w", err)
 	}
 
-	return NewWithSchema[T](schema), nil
+	return NewWithSchema[T](schema)
 }
