ed6cb120a5a6c26b628affc71dacb4abcc302ff0
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-02-17 13:21:05 +0100

    Add gcp

---
+++ pkg/cloud/gcp/gcp.go
@@ -0,0 +1,45 @@
+package gcp
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
+	"github.com/Motmedel/utils_go/pkg/errors/types/empty_error"
+	motmedelHttpUtils "github.com/Motmedel/utils_go/pkg/http/utils"
+
+	"github.com/Motmedel/utils_go/pkg/http/types/fetch_config"
+)
+
+var metadataBaseUrl = url.URL{
+	Scheme: "http",
+	Host:   "metadata.google.internal",
+	Path:   "/computeMetadata/v1",
+}
+
+func GetIdToken(ctx context.Context, audience string) (string, error) {
+	if audience == "" {
+		return "", motmedelErrors.NewWithTrace(empty_error.New("audience"))
+	}
+
+	if err := ctx.Err(); err != nil {
+		return "", fmt.Errorf("context err: %w", err)
+	}
+
+	identityUrl := metadataBaseUrl
+	identityUrl.Path += "/instance/service-accounts/default/identity"
+	identityUrl.RawQuery = url.Values{"audience": {audience}}.Encode()
+
+	identityUrlString := identityUrl.String()
+	_, responseBody, err := motmedelHttpUtils.Fetch(
+		ctx,
+		identityUrlString,
+		fetch_config.WithHeaders(map[string]string{"Metadata-Flavor": "Google"}),
+	)
+	if err != nil {
+		return "", motmedelErrors.New(fmt.Errorf("fetch: %w", err), identityUrlString)
+	}
+
+	return string(responseBody), nil
+}
