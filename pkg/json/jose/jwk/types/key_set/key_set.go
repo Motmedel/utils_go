824490d808026e4dd676d523ada55f472cfb27e3
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-03-09 23:04:54 +0100

    Fix key set json

--- pkg/json/jose/jwk/types/key_set/key_set.go
+++ pkg/json/jose/jwk/types/key_set/key_set.go
@@ -3,5 +3,5 @@ package key_set
 import "github.com/Motmedel/utils_go/pkg/json/jose/jwk/types/key"
 
 type KeySet struct {
-	Keys []*key.Key `json:"keys,omitempty"`
+	Keys []*key.Key `json:"keys"`
 }
