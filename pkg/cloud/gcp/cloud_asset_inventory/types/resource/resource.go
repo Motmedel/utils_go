77effe2cf4f390e539b60743f72b42bad1f3ec4c
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-03-16 18:37:26 +0100

    Change resource data to use RawMessage

--- pkg/cloud/gcp/cloud_asset_inventory/types/resource/resource.go
+++ pkg/cloud/gcp/cloud_asset_inventory/types/resource/resource.go
@@ -1,11 +1,13 @@
 package resource
 
+import "encoding/json"
+
 type Resource struct {
-	Version              string         `json:"version,omitempty"`
-	DiscoveryDocumentUri string         `json:"discoveryDocumentUri,omitempty"`
-	DiscoveryName        string         `json:"discoveryName,omitempty"`
-	ResourceUrl          string         `json:"resourceUrl,omitempty"`
-	Parent               string         `json:"parent,omitempty"`
-	Data                 map[string]any `json:"data,omitempty"`
-	Location             string         `json:"location,omitempty"`
+	Version              string          `json:"version,omitempty"`
+	DiscoveryDocumentUri string          `json:"discoveryDocumentUri,omitempty"`
+	DiscoveryName        string          `json:"discoveryName,omitempty"`
+	ResourceUrl          string          `json:"resourceUrl,omitempty"`
+	Parent               string          `json:"parent,omitempty"`
+	Data                 json.RawMessage `json:"data,omitempty"`
+	Location             string          `json:"location,omitempty"`
 }
